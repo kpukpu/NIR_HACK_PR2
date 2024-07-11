4925f0ff60161382f7be11ec6559d071f8c767e9
Esalyc <yunchancode@naver.com>
2024-07-11 15:56:12 +0900

    프론트 프로젝트 구조 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import RecommendedBook from "./pages/RecommendedBook/RecommendedBook"
+import SearchBook from "./pages/SearchBook/SearchBook"
+import LoanManagement from "./pages/LoanManagement/LoanManagement"
+import BookManagement from "./pages/BookManagement/BookManagement"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recommended-book" element={<RecommendedBook />} />
+          <Route path="/search-book" element={<SearchBook />} />
+          <Route path="/loan-management" element={<LoanManagement />} />
+          <Route path="/book-management" element={<BookManagement />} />
+      </Routes>
+    );
 }
 
 export default App;
