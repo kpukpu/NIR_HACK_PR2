fcb14190beacaa9ebe05f94bc7a308e183468833
kpukpu <57974389+kpukpu@users.noreply.github.com>
2024-07-13 17:37:48 +0900

    insert 추가

--- frontend/src/data/Post.js
+++ frontend/src/data/Post.js
@@ -1,11 +1,7 @@
 import axiosInstance from './AxiosConfig';
 
 export const postBook = async (book) => {
-    try {
-        const response = await axiosInstance.post('/post', book);
-        return response.data;
-    } catch (error) {
-        console.error("There was an error posting the book!", error);
-        throw error;
-    }
+    console.log('Sending book data:', book); // 데이터 확인용 콘솔 로그
+    const response = await axiosInstance.post('post/', book);
+    return response.data;
 };
