ad3fbfe760b46b0c342777e9b39a9a45c371c632
Hye0nw0O <hw4151@naver.com>
2024-07-13 17:11:59 +0900

    add function

--- backend/delete/urls.py
+++ backend/delete/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import delete
+from . import views
 
 urlpatterns = [ 
     path('delete/<int:id>/', delete), # pk = id 값을 book_number로 가지는 book을 Book 모델에서 삭제
+    path('available/', views.available), # 대출 가능 도서 목록
+    path('unavailable/', views.unavailable), # 대출 불가능 도서 목록
 ]
--- backend/delete/views.py
+++ backend/delete/views.py
@@ -3,6 +3,8 @@ from searching.models import Book
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import render
+from django.http import JsonResponse
 # Create your views here.
 
 @api_view(['DELETE']) 
@@ -14,3 +16,12 @@ def delete(request, id):
     except Book.DoesNotExist:
         return Response({'error' : 'Item not exist'}, status=status.HTTP_404_NOT_FOUND)
     
+def available(request):
+    book = Book.objects.filter(availability = True).values('book_name', 'publisher', 'author')
+    book_list = list(book)
+    return JsonResponse(book_list, safe=False)
+
+def unavailable(request):
+    book = Book.objects.filter(availability = False).values('book_name', 'publisher', 'author')
+    book_list = list(book)
+    return JsonResponse(book_list, safe=False)
