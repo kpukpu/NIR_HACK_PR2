7b49c2935f60c1fc1bd192c83e4b7d1a170c9507
kpukpu <57974389+kpukpu@users.noreply.github.com>
2024-07-13 17:35:31 +0900

    insert 추가

--- backend/searching/views.py
+++ backend/searching/views.py
@@ -10,6 +10,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from .models import Book
 from .serializers import BookSerializer
+from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 
 @api_view(['GET'])
 def book_list(request):
@@ -25,3 +27,45 @@ def book_search(request):
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+def book_insert(request):
+    if request.method == 'POST':
+        try:
+            # 수신된 데이터 로깅
+            data = json.loads(request.body)
+            print('Received data:', data)
+
+            # 데이터 파싱
+            book_name = data.get('bookname')
+            author = data.get('auth')
+            publisher = data.get('publish')
+            publication_date = data.get('_date')
+            availability = data.get('avail')
+
+            # Book 객체 생성
+            new_book = Book(
+                book_number = 1,
+                book_name=book_name,
+                publisher=publisher,
+                availability=availability,
+                author=author,
+                publication_date=publication_date,
+            )
+
+            # 데이터 유효성 검사
+            new_book.full_clean()  
+            new_book.save()  # 데이터베이스에 저장
+            
+            return JsonResponse({'message': 'Book added successfully!'}, status=201)
+
+        except ValidationError as e:
+            # 유효성 검사 오류
+            return JsonResponse({'error': e.message_dict}, status=400)
+        except Exception as e:
+            # 기타 오류
+            print('Error:', str(e))
+            return JsonResponse({'error': str(e)}, status=500)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
