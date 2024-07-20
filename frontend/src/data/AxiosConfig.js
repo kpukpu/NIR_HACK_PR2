9da5bb28c00711c058c2504732143d857d5c431f
Esalyc <yunchancode@naver.com>
2024-07-21 01:18:53 +0900

    배포

--- frontend/src/data/AxiosConfig.js
+++ frontend/src/data/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://3.38.214.247:22/:8000/api',
+    baseURL: 'http://127.0.0.1/:8000/api',
     timeout: 5000000,
     headers: {
         'Content-Type': 'application/json',
